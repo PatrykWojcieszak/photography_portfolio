19ad1a3f42225dea83e5a7917e6991029d33c6ef
Patryk Wojcieszak <patryk.wojcieszak@tsh.io>
2023-10-15 11:58:23 +0200

    initial

---
+++ src/app/header/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <div className="flex justify-between py-6 px-12">
+      <h1 className="text-white text-2xl">Patryk Wojcieszak</h1>
+    </div>
+  );
+};
---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  black: "#0A0C0B",
+  blue: "#4B718D",
+  white: "#C5CDCC",
+  gray: "#98A4A4",
+  lightGreen: "#6F8D98",
+  brown: "#443F39",
+};
+
+module.exports = colors;

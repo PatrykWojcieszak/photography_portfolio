ce0b24f07d7455094f1e3f3f281841f3c36f01b7
Patryk Wojcieszak <patryk.wojcieszak@tsh.io>
2023-11-13 19:29:42 +0100

    feature: add different photo size for expanded photo

--- src/components/photo/expandedPhoto/ExpandedPhoto.tsx
+++ src/components/photo/expandedPhoto/ExpandedPhoto.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import { ExpandedPhotoProps } from "./ExpandedPhoto.types";
 import { shimmerLoader } from "@/utils/getShimmerLoader";
+import { PhotoSize } from "@/components/masonryGallery/MasonryGallery.types";
+
+const VERTICAL_PHOTO_WIDTH = 720;
+const HORIZONTAL_PHOTO_WIDTH = 1920;
 
 export const ExpandedPhoto = ({
   closeExpandedMode,
@@ -8,6 +12,7 @@ export const ExpandedPhoto = ({
   description,
   isPhotoLoaded,
   onPhotoLoaded,
+  size,
 }: ExpandedPhotoProps) => {
   const onLoadedImage = (image: HTMLImageElement) => {
     onPhotoLoaded(true);
@@ -26,7 +31,13 @@ export const ExpandedPhoto = ({
           alt={description}
           fill
           style={{ objectFit: "contain" }}
-          src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${photoId}.webp`}
+          src={`https://res.cloudinary.com/${
+            process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
+          }/image/upload/c_scale,w_${
+            size !== PhotoSize.VERTICAL
+              ? HORIZONTAL_PHOTO_WIDTH
+              : VERTICAL_PHOTO_WIDTH
+          }/${photoId}.webp`}
           placeholder={`data:image/svg+xml;base64,${shimmerLoader}`}
           className="transition-opacity opacity-0 duration-[0.5s] rounded-lg"
           onLoadingComplete={onLoadedImage}

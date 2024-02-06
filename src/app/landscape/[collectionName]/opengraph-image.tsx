ea5dfe59c171ae595b1beb7ef125322754bdd377
Patryk Wojcieszak <patryk.wojcieszak@tsh.io>
2024-02-06 17:54:52 +0100

    feature: opengraph test

--- src/app/landscape/[collectionName]/opengraph-image.tsx
+++ src/app/landscape/[collectionName]/opengraph-image.tsx
@@ -1,5 +1,6 @@
 import BaseImage from "next/image";
 import { ImageResponse } from "next/og";
+import { NextRequest } from "next/server";
 
 // Route segment config
 export const runtime = "edge";
@@ -14,27 +15,19 @@ export const size = {
 export const contentType = "image/png";
 
 // Image generation
-export default async function Image({
-  params,
-}: {
-  params: {
-    collectionName: string;
-    searchParams: {
-      photo: string;
-    };
-  };
-}) {
+export default async function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  const photoId = searchParams.get("photo");
+
   return new ImageResponse(
     (
       // ImageResponse JSX element
       <BaseImage
         unoptimized
-        src={`https://res.cloudinary.com/${
-          process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
-        }/image/upload/c_scale,h_500/${
-          params.searchParams.photo ?? "mzgshobomvwz7w4cica9"
-        }.webp`}
+        src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,h_500/${photoId}.webp`}
         alt={alt}
+        width={size.width}
+        height={size.height}
         className="border border-white/50 rounded-lg cursor-pointer"
       />
     ),

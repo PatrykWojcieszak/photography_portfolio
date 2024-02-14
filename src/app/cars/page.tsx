87150cdf081ea1a9938951c49dc06efcea1d091f
Patryk Wojcieszak <patryk.wojcieszak@tsh.io>
2024-02-14 18:04:41 +0100

    feat: add og image to category

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -3,16 +3,24 @@ import { Metadata } from "next";
 import { fetchImageGallery } from "../api/actions/fetchImageGallery";
 import { GalleryContextController } from "@/providers/gallery/galleryContextController/GalleryContextController";
 import { headers } from "next/headers";
+import { fetchCategories } from "../api/actions/fetchCategories";
 
 const COLLECTION_NAME = "cars";
 
 export async function generateMetadata({
+  params,
   searchParams,
 }: {
+  params: { collectionName: string };
   searchParams: {
     photo: string;
   };
 }): Promise<Metadata> {
+  const categories = await fetchCategories("homePage");
+  const categoryPhoto = categories.find(
+    (category) => category.collectionName === params.collectionName
+  )?.thumbnailId;
+
   const headersList = headers();
   const pathname = headersList.get("x-invoke-path") || "";
 
@@ -21,7 +29,9 @@ export async function generateMetadata({
     openGraph: {
       images: [
         {
-          url: `${pathname.split("/")[0]}/api/og?photo=${searchParams.photo}`,
+          url: `${pathname.split("/")[0]}/api/og?photo=${
+            searchParams.photo ?? categoryPhoto
+          }`,
           width: 1200,
           height: 630,
           alt: "photo thumbnail",
--- src/app/landscape/[collectionName]/page.tsx
+++ src/app/landscape/[collectionName]/page.tsx
@@ -16,6 +16,11 @@ export async function generateMetadata({
     photo: string;
   };
 }): Promise<Metadata> {
+  const categories = await fetchCategories("homePage");
+  const categoryPhoto = categories.find(
+    (category) => category.collectionName === params.collectionName
+  )?.thumbnailId;
+
   const headersList = headers();
   const pathname = headersList.get("x-invoke-path") || "";
 
@@ -24,7 +29,9 @@ export async function generateMetadata({
     openGraph: {
       images: [
         {
-          url: `${pathname.split("/")[0]}/api/og?photo=${searchParams.photo}`,
+          url: `${pathname.split("/")[0]}/api/og?photo=${
+            searchParams.photo ?? categoryPhoto
+          }`,
           width: 1200,
           height: 630,
           alt: "photo thumbnail",

548ac0f5017973a63f349d1377cab5214e4733c1
Patryk Wojcieszak <patryk.wojcieszak@tsh.io>
2024-03-09 16:55:23 +0100

    remove metadata and static page generation

--- src/app/landscape/[collectionName]/[photo]/page.tsx
+++ src/app/landscape/[collectionName]/[photo]/page.tsx
@@ -1,27 +1,5 @@
 import { fetchImageGallery } from "@/app/api/actions/fetchImageGallery";
 import { ExpandedPhotoWithGallery } from "@/components/expandedPhotoWithGallery/ExpandedPhotoWithGallery";
-import { getPageTitleFromCollectionName } from "@/utils/getPageTitleFromCollectionName";
-import { Metadata } from "next";
-
-export function generateMetadata({
-  params,
-}: {
-  params: { collectionName: string; photo: string };
-}): Metadata {
-  return {
-    title: getPageTitleFromCollectionName(params.collectionName),
-    openGraph: {
-      images: [
-        {
-          url: `/api/og?photo=${params.photo}`,
-          width: 1200,
-          height: 630,
-          alt: "photo thumbnail",
-        },
-      ],
-    },
-  };
-}
 
 const getData = async (collectionName: string) => {
   return await fetchImageGallery(collectionName);
